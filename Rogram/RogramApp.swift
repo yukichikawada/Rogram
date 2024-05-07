c942d6b389d1449111ba7264fdc817147c6b5d6c
Yukichi Kawada <yukichikawada@gmail.com>
2024-05-07 12:05:40 -0400

    Initial Commit

---
+++ Rogram/RogramApp.swift
@@ -0,0 +1,17 @@
+//
+//  RogramApp.swift
+//  Rogram
+//
+//  Created by Yukichi Kawada on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct RogramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RogramUITests/RogramUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RogramUITestsLaunchTests.swift
+//  RogramUITests
+//
+//  Created by Yukichi Kawada on 5/7/24.
+//
+
+import XCTest
+
+final class RogramUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
